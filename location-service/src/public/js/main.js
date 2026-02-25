c143c4c7cf488049b6f2114ed9d71250b88ce0d1
jmartinez-2024083 <jmartinez-2024083@kinal.edu.gt>
2026-02-25 00:08:03 -0600

    feat: agregar marcador dinámico para ubicación del bus.

--- location-service/src/public/js/main.js
+++ location-service/src/public/js/main.js
@@ -4,6 +4,22 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+map.locate({ enableHighAccuracy: true });
+map.on('locationfound', e => {
+  const coords = [e.latlng.lat, e.latlng.lng];
+
+  L.marker(coords, { icon: busIcon })
+    .addTo(map)
+    .bindPopup('Ubicación actual del bus');
+});
+
 L.marker([14.6258, -90.5360])
   .addTo(map)
   .bindPopup('Bienvenido a Kinal!');
+
+  const busIcon = L.divIcon({
+  html: "🚌",
+  className: "bus-icon",
+  iconSize: [30, 30],
+  iconAnchor: [15, 15]
+});
