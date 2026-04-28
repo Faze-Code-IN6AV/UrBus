45cbb5e41e7b4bf6636a75d93fe2143da49fe703
dvelasquez-2024226 <dvelasquez-2024226@kinal.edu.gt>
2026-04-27 20:50:45 -0600

    feat: se agregó la documentación del notification-service

--- notification-service/configs/app.js
+++ notification-service/configs/app.js
@@ -5,23 +5,22 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import notificationRoutes from '../src/notifications/notification.routes.js';
+import swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import { parse } from 'yaml';
 
 const BASE_PATH = '/urbus/v1';
+const swaggerDoc = parse(readFileSync('./swagger.yaml', 'utf8'));
 
-export const initServer = async () => {
-    const app = express();
-    const PORT = process.env.PORT || 5000;
-
-    app.set('trust proxy', 1);
-
-    // Middlewares
+const middlewares = (app) => {
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use(cors());
     app.use(morgan('dev'));
     app.use(helmet());
+};
 
-    // Routes
+const routes = (app) => {
     app.use(`${BASE_PATH}/notifications`, notificationRoutes);
 
     app.get(`${BASE_PATH}/health`, (req, res) => {
@@ -37,10 +36,33 @@ export const initServer = async () => {
             message: 'Ruta no encontrada en el servidor.'
         });
     });
+};
 
-    // Start server
-    app.listen(PORT, () => {
-        console.log(`Servicio de notificación ejecutándose en puertot: ${PORT}`);
-        console.log(`Health Check: http://localhost:${PORT}${BASE_PATH}/health`);
-    });
+export const initServer = async () => {
+    const app = express();
+    const PORT = process.env.PORT || 5000;
+    app.set('trust proxy', 1);
+
+    try {
+        middlewares(app);
+        routes(app);
+        app.use(`${BASE_PATH}/swagger`, swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+        // 404 siempre al último
+        app.use((req, res) => {
+            res.status(404).json({
+                success: false,
+                message: 'Ruta no encontrada en el servidor.'
+            });
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Servicio de notificación ejecutándose en puerto: ${PORT}`);
+            console.log(`Health Check: http://localhost:${PORT}${BASE_PATH}/health`);
+        });
+
+    } catch (err) {
+        console.error(`Error al iniciar el servidor: ${err.message}`);
+        process.exit(1);
+    }
 };
