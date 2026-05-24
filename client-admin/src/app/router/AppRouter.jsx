aab45c39088094b17b7a195938f5eb7b68109d92
dvelasquez-2024226 <dvelasquez-2024226@kinal.edu.gt>
2026-05-23 21:51:37 -0600

    feat: agregar navegación y layout base con sidebar.

--- client-admin/src/app/router/AppRouter.jsx
+++ client-admin/src/app/router/AppRouter.jsx
@@ -9,6 +9,7 @@ import { LocationPage } from '../../features/location/pages/LocationPage.jsx';
 import { PassengerListPage } from '../../features/passangers/pages/PassengerListPage.jsx';
 //import { VerifyEmailPage } from '../../features/auth/pages/VerifyEmailPage.jsx';
 //import { ResetPasswordPage } from '../../features/auth/pages/ResetPasswordPage.jsx';
+import { PostListPage } from '../../features/posts/pages/PostListPage.jsx';
 
 export const AppRouter = () => {
   return (
@@ -29,6 +30,7 @@ export const AppRouter = () => {
         <Route index element={<LocationPage />} />
         <Route path='location' element={<LocationPage />} />
         <Route path='passengers' element={<PassengerListPage />} />
+        <Route path='posts' element={<PostListPage />} />
       </Route>
 
       <Route path='*' element={<Navigate to='/dashboard' replace />} />
